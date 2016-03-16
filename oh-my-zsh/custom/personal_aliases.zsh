55786b85d0b98e22b085926d222afc7554d5c939
olitreadwell <olitreadwell@gmail.com>
2016-03-16 11:20:24 -0500

    remove Union Metrics aliases

--- oh-my-zsh/custom/personal_aliases.zsh
+++ oh-my-zsh/custom/personal_aliases.zsh
@@ -15,23 +15,6 @@ alias edit="open -a 'Sublime Text 2'" #open specified file in Sublime Text
 # Git Shortcuts
 alias gitprepdeploy='git fetch --all; git checkout master; git pull --rebase; git checkout development; git pull --rebase'
 
-# Union Metrics Specific
-alias umw='cd ~/UM/Union-Web'   #go to UMW
-alias trw='cd ~/UM/TweetReach-Web' #go to TRW
-alias trvm='cd ~/UM/TRVM/dist; ' #open TRVM
-alias "trvm-up"='cd ~/UM/TRVM/dist; vagrant up; vagrant ssh' 
-alias "trvm-halt"='cd ~/UM/TRVM/dist; vagrant halt' #open TRV
-alias "trvm-reload"='cd ~/UM/TRVM/dist; vagrant reload' #open TRVMb
-
-alias tw4='ssh appozite@tweetreach-w4-01.union.corp; ' #open tweetreach-w4-01 << workers!
-alias tr1='ssh appozite@tweetreach-rails1.union.corp; ' #open tweetreach-rails1 << these replace the leelas
-alias tr2='ssh appozite@tweetreach-rails2.union.corp; ' #open tweetreach-rails2
-
-alias u1='ssh appozite@union-rails1.union.corp; ' #open union-rails1
-alias u2='ssh appozite@union-rails2.union.corp; ' #open union-rails2
-alias l3='ssh appozite@leela3.union.corp; ' #open leela3
-alias l4='ssh appozite@leela4.union.corp; ' #open leela4
-alias l8='ssh appozite@leela8.union.corp; ' #open leela8
 
 # Emacs Aliases
 alias "e."='emacs .' # open current directory in emacs

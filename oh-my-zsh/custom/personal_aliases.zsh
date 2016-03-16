f9d873a984d2d4198d71deffa9fbddbe25725f69
olitreadwell <olitreadwell@gmail.com>
2016-03-16 11:20:39 -0500

    remove unused pick shortcuts

--- oh-my-zsh/custom/personal_aliases.zsh
+++ oh-my-zsh/custom/personal_aliases.zsh
@@ -5,12 +5,6 @@ alias edit_host="emacs /etc/hosts" #open hosts filed for editing
 alias edit_aliases="emacs ~/.oh-my-zsh/custom/personal_aliases.zsh" #open personal aliases file in emacs
 alias edit="open -a 'Sublime Text 2'" #open specified file in Sublime Text
 
-# Pick
-
-# TODO these were running on opening a new tab and then not taking effect
-
-# alias cdpick="cd $(find . -type d | pick) && l" #go to directory using pick
-# alias gcopick="git checkout $(git branch | cut -c 3- | pick)" #switch git branch with pick
 
 # Git Shortcuts
 alias gitprepdeploy='git fetch --all; git checkout master; git pull --rebase; git checkout development; git pull --rebase'

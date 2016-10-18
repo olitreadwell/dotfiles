098b9e512f5374e6acb71e75a5c9b47710586eff
Oli Treadwell <oliver.treadwell@gmail.com>
2016-10-18 14:34:46 -0500

    include alias and zshrc for Galvanize

--- oh-my-zsh/custom/personal_aliases.zsh
+++ oh-my-zsh/custom/personal_aliases.zsh
@@ -11,6 +11,7 @@ alias rbm.="open -a 'RubyMine' ."
 alias gitprepdeploy='git fetch --all; git checkout master; git pull --rebase; git checkout development; git pull --rebase'
 
 # Emacs Aliases
+# alias emacs="/usr/local/Cellar/emacs/24.5/Emacs.app/Contents/MacOS/Emacs -nw"
 alias "e."='emacs .' # open current directory in emacs
 alias "e"='emacs'    # open target file in emacs
 alias es='emacs'     # open scratch folder in emacs
@@ -22,3 +23,6 @@ alias berc='bundle exec rails console'
 #SourceTree
 alias "sct."="open -a 'SourceTree' ." # open current directory in SourceTree
 alias "sct"="open -a 'SourceTree'"    # open target file in SourceTree
+
+#Galvanize
+alias grading='cd ~/code/galvanize/g33/grading'
